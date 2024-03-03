c70b00200a349126ede4bbb07601b802ebd6a99a
bapakfadil <sipadil28@hotmail.com>
2024-03-03 22:04:36 +0700

    Redefined: Post detail page styling

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,11 +1,21 @@
 @extends('layout.main')
 
+{{-- Post Details Page --}}
 @section('mainSection')
-    <article class="mb-3">
-        <h2>{{ $post['postTitle'] }}</h2>
-        <h6>By : <a href="/author/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h6>
-        {!! $post->postBody !!}
-    </article>
 
-    <span>Back to <a href="/posts" class="text-decoration-none">Posts</a></span>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <article class="mb-4">
+                    <h2 class="mb-3"><strong>{{ $post['postTitle'] }}</strong></h2>
+                    <h6 class="mb-3">By <a href="/author/{{ $post->author->username }}" class="text-decoration-none text-dark">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none text-dark">{{ $post->category->name }}</a></h6>
+
+                    {!! $post->postBody !!}
+                </article>
+
+                <a href="/posts" class="text-decoration-none btn btn-sm btn-secondary bg-dark">Back to Posts</a>
+            </div>
+        </div>
+    </div>
+
 @endsection
