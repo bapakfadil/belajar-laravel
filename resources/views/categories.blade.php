612eea906d9040281019eb9eaf7d2d73c4a2a062
bapakfadil <sipadil28@hotmail.com>
2024-03-07 20:59:06 +0700

    Redefined Categories page view

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -3,15 +3,26 @@
 @section('mainSection')
     <h1 class="mb-5">Post Categories</h1>
 
-    @foreach ($categories as $category)
-        <ul>
-            <li>
-                <h3><a href="/categories/{{ $category->slug }}">{{ $category->name }}</a></h3>
-            </li>
-        </ul>
-    @endforeach
+    <div class="container">
+        <div class="row">
+            @foreach ( $categories as $category )
+            <div class="col-md-4">
+                <a href="/categories/{{ $category->slug }}">
+                    <div class="card text-bg-dark">
+                        <img src="https://source.unsplash.com/500x300?{{ $category->name }}" class="card-img" alt="...">
+                        <div class="card-img-overlay">
+                            <div class="position-absolute m-0 px-3 py-2" style="background-color: rgba(0,0,0, 0.7)">
+                                <h5 class="card-title">{{ $category->name}}</h5>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            @endforeach
+        </div>
+    </div>
 
-    <div class="row category-footer pt-3 pb-5">
+    <div class="row category-footer pt-5 pb-5">
         <div class="col">
             <a href="/posts" class="text-decoration-none btn btn-sm btn-outline-dark">Back to Posts</a>
         </div>
