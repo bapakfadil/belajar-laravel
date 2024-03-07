ed89f8ed979f34b489601653480e21541c3ee4f2
bapakfadil <sipadil28@hotmail.com>
2024-03-07 11:15:03 +0700

    Modified PostFactory from standard faker paragraph into Structured Multiple Paragraph with <p> tags

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -16,10 +16,19 @@ class PostFactory extends Factory
         return [
             'postTitle' => $this->faker->sentence(mt_rand(2,6)),
             'category_id' => mt_rand(1,3),
-            'user_id' => mt_rand(1,5),
+            'user_id' => mt_rand(1,5), // modify this line if you're making less than 5 user account in database seeder
             'slug' => $this->faker->slug(),
             'excerpt' => $this->faker->paragraph(),
-            'postBody' => $this->faker->paragraph(mt_rand(5,10))
+            // Example of use using standard faker paragraph
+            //'postBody' => $this->faker->paragraph(mt_rand(5,10))
+
+            // Example of use using implode()
+            //'postBody' => '<p>' . implode('</p><p>', $this->faker->paragraphs(mt_rand(5,10))) . '</p>',
+
+            // Example of use using map()
+            'postBody' => collect($this->faker->paragraphs(mt_rand(5,10)))->map(fn($p) => "<p>$p</p>")->implode(''),
+
+
         ];
     }
 }
