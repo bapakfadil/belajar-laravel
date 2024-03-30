2436b82619dbf61186f7abf33659b7bf61c89bb2
bapakfadil <sipadil28@hotmail.com>
2024-03-30 12:55:35 +0700

    Created Dashboard UI

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,1 +1,7 @@
-<h1>Welcome, selamat datang</h1>
+@extends('dashboard.layouts.main')
+
+@section('mainContainer')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Welcome back, {{ auth()->user()->name }}</h1>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\DashboardController;
 
 // Halaman Utama
 Route::get('/', function () {
@@ -49,7 +48,9 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store'])->middleware('guest');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function(){
+    return view('dashboard.index');
+})->middleware('auth');
 
 // unused, replaced by Category Query at Post Model
 /*
